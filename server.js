5f347a29343bb22396e883378567d4ac473710af
komallsingh <singhkomal11711@gmail.com>
2026-06-27 18:30:55 +0530

    render error check

--- server.js
+++ server.js
@@ -104,7 +104,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/auth", authRoutes);
-
+console.log("process.env.NODE_ENV", process.env.NODE_ENV);
+console.log("process.env.MONGO_URI", process.env.MONGO_URI);
+console.log("process.env.JWT_SECRET", process.env.JWT_SECRET);
 
 if (process.env.NODE_ENV !== "test") {
   connectDB();
